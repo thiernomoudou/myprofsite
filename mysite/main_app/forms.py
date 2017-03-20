86fcf5f91c5de1d1511776cb41328d738b7a299b
thiernomoudou <souleymanemoudou@gmail.com>
2017-03-20 10:02:33 +0000

    add form validation

--- mysite/main_app/forms.py
+++ mysite/main_app/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-control form-element'}), help_text='Your Name')
-    email = forms.EmailField(required=False, widget=forms.TextInput(attrs={'class': 'form-control form-element'}))
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control form-element'}), help_text='Your Name')
+    email = forms.EmailField( widget=forms.TextInput(attrs={'class': 'form-control form-element'}))
     message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control form-element'}))
--- mysite/main_app/models.py
+++ mysite/main_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class Contact(models.Model):
     name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
     message = models.CharField(max_length=2000)
 
     def __str__(self):
